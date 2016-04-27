1b85271bea1efc2188ff051e4bc851534b30fe52
Luke Miller <millerlj@rose-hulman.edu>
2016-04-27 09:40:32 -0400

    working on refactoring moves

--- src/move_commands/MoveCommand.java
+++ src/move_commands/MoveCommand.java
@@ -1,20 +1,60 @@
 package move_commands;
 
 import game.BoardState;
+import game.Coordinate;
+import piece.Owner;
 
-public interface MoveCommand {
+public abstract class MoveCommand {
 
-	public BoardState execute();
-	
-	public BoardState getOriginalBoard();
-	
+
+    Owner turn;
+
+    protected BoardState originalBoard;
+
+    abstract public BoardState execute();
+
+    abstract public BoardState getOriginalBoard();
+
+    abstract public boolean isValidMove();
+
+    protected boolean isFrozen(Coordinate pieceToMove) {
+        if (!isNextToStrongerPiece(pieceToMove, this.turn)
+                && isNextToStrongerPiece(pieceToMove, this.getOtherOwner())) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isNextToStrongerPiece(Coordinate pieceToMove, Owner player) {
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                Coordinate coor = new Coordinate(i + pieceToMove.getX(), j + pieceToMove.getY());
+                if (this.originalBoard.getPieceAt(coor) != null) {
+                    if (coor.isValid() && !coor.equals(pieceToMove)
+                            && this.originalBoard.getPieceAt(coor).getOwner() == player
+                            && this.originalBoard.getPieceAt(coor).isStrongerThan(this.originalBoard.getPieceAt(pieceToMove))) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    public Owner getOtherOwner() {
+        if (turn == Owner.values()[0]) {
+            return Owner.values()[1];
+        } else {
+            return Owner.values()[0];
+        }
+    }
+
 //	public boolean isValidMove();
-	
+
 //	public static boolean validMove(BoardState originalBoard, int row, int column) {
 //		if (row >= 0 && row < 8 && column >= 0 && column < 8
 //				&& originalBoard.getBoardArray()[row][column] == ' ')
 //			return true;
 //		return false;
 //	}
-	
+
 }
--- src/move_commands/RegularMove.java
+++ src/move_commands/RegularMove.java
@@ -2,15 +2,18 @@ package move_commands;
 
 import game.BoardState;
 import game.Coordinate;
+import piece.AbstractPiece;
+import piece.Owner;
+import piece.Rabbit;
 
-public class RegularMove implements MoveCommand {
+public class RegularMove extends MoveCommand {
 
-	private BoardState originalBoard;
 	private BoardState newBoard;
 	private Coordinate originalPlace;
 	private Coordinate newPlace;
 
-	public RegularMove(BoardState board, Coordinate originalPlace, Coordinate newPlace) {
+	public RegularMove(BoardState board, Coordinate originalPlace, Coordinate newPlace, Owner turn) {
+		this.turn = turn;
 		this.originalBoard = board.clone();
 		this.newBoard = board;
 		this.originalPlace = originalPlace;
@@ -19,13 +22,46 @@ public class RegularMove implements MoveCommand {
 
 	@Override
 	public BoardState execute() {
+		AbstractPiece piece = this.originalBoard.getPieceAt(originalPlace);
+		if (isFrozen(originalPlace)) {
+			return originalBoard;
+		}
+		if ((piece instanceof Rabbit)) {
+			if (((piece.getOwner() == Owner.values()[0]) && (newPlace.equals(originalPlace.up())))
+					|| ((piece.getOwner() == Owner.values()[1]) && (newPlace.equals(originalPlace.down())))) {
+				// Cannot move a Rabbit backwards unless it has been dragged
+				return this.originalBoard;
+			}
+		}
+		if (originalPlace.isOrthogonallyAdjacentTo(newPlace)
+				&& this.originalBoard.getPieceAt(originalPlace).getOwner() == this.turn) {
+				return this.originalBoard;
+		}
 		this.newBoard.movePiece(originalPlace, newPlace);
 		return newBoard;
 	}
 
+
+
 	@Override
 	public BoardState getOriginalBoard() {
 		return originalBoard;
+	}
+
+	@Override
+	public boolean isValidMove() {
+		AbstractPiece piece = this.originalBoard.getPieceAt(originalPlace);
+		if (isFrozen(originalPlace)) {
+			return false;
+		}
+		if ((piece instanceof Rabbit)) {
+			if (((piece.getOwner() == Owner.values()[0]) && (newPlace.equals(originalPlace.up())))
+					|| ((piece.getOwner() == Owner.values()[1]) && (newPlace.equals(originalPlace.down())))) {
+				// Cannot move a Rabbit backwards unless it has been dragged
+				return false;
+			}
+		}
+		return true;
 	}
 
 //	@Override
