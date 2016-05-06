2d8d55d36aa68b0808d9a94e6011e23cb6172776
Philip Ross <rosspa@rose-hulman.edu>
2016-05-06 14:12:00 -0400

    owner enum now knows the other owner

--- src/piece/Owner.java
+++ src/piece/Owner.java
@@ -1,6 +1,15 @@
 package piece;
 
 public enum Owner {
-	Player1, Player2
-}
+	Player1, Player2;
+	private Owner otherOwner;
 
+	static {
+		Player1.otherOwner = Player2;
+		Player2.otherOwner = Player1;
+	}
+
+	public Owner getOtherOwner() {
+		return this.otherOwner;
+	}
+}
