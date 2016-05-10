8e5bde5260555077bfa582d8d1476c03fdacb706
Philip Ross <rosspa@rose-hulman.edu>
2016-05-10 15:53:39 -0400

    wrote tests for checking if regular moves are possible

---
+++ test/game/TestNoMoreMoves.java
@@ -0,0 +1,20 @@
+package game;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import board.BoardState;
+import board.Coordinate;
+import piece.AbstractPiece;
+import piece.Dog;
+import piece.Owner;
+import piece.Rabbit;
+
+public class TestNoMoreMoves {
+
+
+}
