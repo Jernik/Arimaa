2ff0d9e5f618c6d3232d2cb0a0cccdf4d25428c5
Philip Ross <rosspa@rose-hulman.edu>
2016-05-10 20:02:51 -0400

    changed test runner to call all our suites

--- src/test_runner/AllTests.java
+++ src/test_runner/AllTests.java
@@ -4,21 +4,26 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import ai.TestAi;
+import ai.TestAiSuite;
 import ai.TestGenerateRandomMoveIsUniform;
 import ai.TestGenerateRandomMoveIsValid;
 import ai.TestGenerateRandomMoveOnlyIfEnoughMoves;
 import ai.TestGenerateRandomMovePerformance;
 import board.TestBoardState;
+import board.TestBoardSuite;
 import board.TestCoordinate;
 import game.TestEndGame;
 import game.TestGame;
 import game.TestGameEquality;
 import game.TestGameHashCodeEquality;
+import game.TestGameSuite;
 import game.TestUndo;
+import gui.TestGuiSuite;
 import gui.TestLoad;
 import gui.TestSave;
 import gui.TestTimePanel;
 import move_commands.TestMoveCommandEquality;
+import move_commands.TestMoveCommandSuite;
 import move_commands.TestPullInvalid;
 import move_commands.TestPullValid;
 import move_commands.TestPushInvalid;
@@ -27,14 +32,11 @@ import move_commands.TestRegularInvalid;
 import move_commands.TestRegularValid;
 import piece.TestPiece;
 import piece.TestPieceEquality;
+import piece.TestPieceSuite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ TestAi.class, TestGenerateRandomMoveIsUniform.class, TestGenerateRandomMoveIsValid.class,
-		TestGenerateRandomMoveOnlyIfEnoughMoves.class, TestGenerateRandomMovePerformance.class, TestBoardState.class,
-		TestCoordinate.class, TestEndGame.class, TestGame.class, TestGameEquality.class, TestGameHashCodeEquality.class,
-		TestUndo.class, TestLoad.class, TestSave.class, TestTimePanel.class, TestMoveCommandEquality.class,
-		TestPullInvalid.class, TestPullValid.class, TestPushValid.class, TestPushInvalid.class,
-		TestRegularInvalid.class, TestRegularValid.class, TestPiece.class, TestPieceEquality.class })
+@Suite.SuiteClasses({ TestAiSuite.class, TestBoardSuite.class, TestGameSuite.class, TestGuiSuite.class,
+		TestMoveCommandSuite.class, TestPieceSuite.class})
 public class AllTests {
 	public static void main(String args[]) {
 		org.junit.runner.JUnitCore.main("test_runner.AllTests");
